9c10f0cc521373b0e901e8eb3c6931dc4f2c2119
VickyPrado <vicky.prado05@gmail.com>
2025-03-26 13:45:12 -0300

    add-MenuAvaliarAlbum

--- ScreenSound/Modelos/Album.cs
+++ ScreenSound/Modelos/Album.cs
@@ -1,23 +1,36 @@
 ﻿namespace ScreenSound.Modelos;
 
-internal class Album
+internal class Album : IAvaliavel
 {
-    public static int ContadorDeObjetos = 0;
     private List<Musica> musicas = new List<Musica>();
+    private List<Avaliacao> notas = new();
 
     public Album(string nome)
     {
         Nome = nome;
-        ContadorDeObjetos++;
     }
 
     public string Nome { get; }
     public int DuracaoTotal => musicas.Sum(m => m.Duracao);
     public List<Musica> Musicas => musicas;
 
+    public double Media
+    {
+        get
+        {
+            if (notas.Count == 0) return 0;
+            else return notas.Average(a => a.Nota);
+        }
+    }
+
     public void AdicionarMusica(Musica musica)
     {
         musicas.Add(musica);
+    }
+
+    public void AdicionarNota(Avaliacao nota)
+    {
+        notas.Add(nota);
     }
 
     public void ExibirMusicasDoAlbum()
