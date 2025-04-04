8e5c33d342587230da0cf14be6eb1fca80c6abf5
VickyPrado <vicky.prado05@gmail.com>
2025-04-04 14:45:39 -0300

    realizando-requisicao

--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -1,13 +1,5 @@
-﻿Episodio ep1 = new(1, "Técnicas de facilitação", 45);
-ep1.AdicionarConvidados("Maria");
-ep1.AdicionarConvidados("Marcelo");
-
-Episodio ep2 = new(2, "Técnicas de aprendizado", 67);
-ep2.AdicionarConvidados("Fernando");
-ep2.AdicionarConvidados("Marcos");
-ep2.AdicionarConvidados("Flávia");
-
-Podcast podcast = new("Podcast especial", "Daniel");
-podcast.AdicionarEpisodio(ep1);
-podcast.AdicionarEpisodio(ep2);
-podcast.ExibirDetalhes();
+﻿using (HttpClient client = new HttpClient())
+{
+    string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+    Console.WriteLine(resposta);
+}
