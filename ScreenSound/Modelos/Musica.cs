6c378cff47ceec5cc5a67cb8bbfeb97cda6d8e08
VickyPrado <vicky.prado05@gmail.com>
2025-04-15 22:29:35 -0300

    desafio5-traduzir-key-para-tonalidade

--- ScreenSound/Modelos/Musica.cs
+++ ScreenSound/Modelos/Musica.cs
@@ -6,7 +6,7 @@ internal class Musica
 {
     [JsonPropertyName("song")]
     public string? Nome { get; set; }
-    
+
     [JsonPropertyName("artist")]
     public string? Artista { get; set; }
 
@@ -19,6 +19,9 @@ internal class Musica
     [JsonPropertyName("year")]
     public string? AnoString { get; set; }
 
+    [JsonPropertyName("key")]
+    public int? Chave { get; set; }
+
     public int Ano
     {
         get
@@ -27,6 +30,7 @@ internal class Musica
         }
     }
 
+    public string? ChaveMusical => Chave?.ToString();
 
     public void ExibirFichaTecnica()
     {
@@ -35,6 +39,50 @@ internal class Musica
         Console.WriteLine($"Duração em segundos: {Duracao / 1000}");
         Console.WriteLine($"Gênero musical: {Genero}");
         Console.WriteLine($"Ano de lançamento: {Ano}");
+        Console.WriteLine($"Chave: {Chave}");
+
+        switch (Chave)
+        {
+            case 0:
+                Console.WriteLine("Chave musical: C");
+                break;
+            case 1:
+                Console.WriteLine("Chave musical: C#");
+                break;
+            case 2:
+                Console.WriteLine("Chave musical: D");
+                break;
+            case 3:
+                Console.WriteLine("Chave musical: D#");
+                break;
+            case 4:
+                Console.WriteLine("Chave musical: E");
+                break;
+            case 5:
+                Console.WriteLine("Chave musical: F");
+                break;
+            case 6:
+                Console.WriteLine("Chave musical: F#");
+                break;
+            case 7:
+                Console.WriteLine("Chave musical: G");
+                break;
+            case 8:
+                Console.WriteLine("Chave musical: G#");
+                break;
+            case 9:
+                Console.WriteLine("Chave musical: A");
+                break;
+            case 10:
+                Console.WriteLine("Chave musical: A#)");
+                break;
+            case 11:
+                Console.WriteLine("Chave musical: B");
+                break;
+            default:
+                Console.WriteLine("Chave desconhecida");
+                break;
+        }        
     }
 }
 
--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -9,33 +9,42 @@ using (HttpClient client = new HttpClient())
     {
         string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");        
         var musicas = JsonSerializer.Deserialize<List<Musica>>(resposta)!;
+
+        foreach (var musica in musicas)
+        {
+            musica.ExibirFichaTecnica();
+            Console.WriteLine(); 
+        }
+        
+
+
         //LinqFilter.FiltrarTodosOsGenerosMusicais(musicas);
         //LinqOrder.ExibirListaDeArtistasOrdenados(musicas);
         //LinqFilter.FiltrarArtistasPorGeneroMusical(musicas, "Rock");
         //LinqFilter.FiltrarMusicasDeUmArtista(musicas, "Skillet");        
         //LinqFilter.FiltrarMusicasPorAno(musicas, 2012);
 
-        var musicasPreferidasDaVicky = new MusicasPreferidas("Vicky");
-        musicasPreferidasDaVicky.AdicionarMusicasFavoritas(musicas[18]);
-        musicasPreferidasDaVicky.AdicionarMusicasFavoritas(musicas[258]);
-        musicasPreferidasDaVicky.AdicionarMusicasFavoritas(musicas[725]);
-        musicasPreferidasDaVicky.AdicionarMusicasFavoritas(musicas[322]);
-        musicasPreferidasDaVicky.AdicionarMusicasFavoritas(musicas[1588]);
-        
-        musicasPreferidasDaVicky.ExibirMusicasFavoritas();
-        //musicasPreferidasDaVicky.GerarArquivoJson();
-        musicasPreferidasDaVicky.GerarArquivoTxt();
+        //var musicasPreferidasDaVicky = new MusicasPreferidas("Vicky");
+        //musicasPreferidasDaVicky.AdicionarMusicasFavoritas(musicas[18]);
+        //musicasPreferidasDaVicky.AdicionarMusicasFavoritas(musicas[258]);
+        //musicasPreferidasDaVicky.AdicionarMusicasFavoritas(musicas[725]);
+        //musicasPreferidasDaVicky.AdicionarMusicasFavoritas(musicas[322]);
+        //musicasPreferidasDaVicky.AdicionarMusicasFavoritas(musicas[1588]);
 
-        var musicasPreferidasDoLeo = new MusicasPreferidas("Léo");
-        musicasPreferidasDoLeo.AdicionarMusicasFavoritas(musicas[487]);
-        musicasPreferidasDoLeo.AdicionarMusicasFavoritas(musicas[572]);
-        musicasPreferidasDoLeo.AdicionarMusicasFavoritas(musicas[18]);
-        musicasPreferidasDoLeo.AdicionarMusicasFavoritas(musicas[666]);
-        musicasPreferidasDoLeo.AdicionarMusicasFavoritas(musicas[1119]);
-        
-        musicasPreferidasDoLeo.ExibirMusicasFavoritas();
-        //musicasPreferidasDoLeo.GerarArquivoJson();
-        musicasPreferidasDoLeo.GerarArquivoTxt();
+        //musicasPreferidasDaVicky.ExibirMusicasFavoritas();
+        ////musicasPreferidasDaVicky.GerarArquivoJson();
+        //musicasPreferidasDaVicky.GerarArquivoTxt();
+
+        //var musicasPreferidasDoLeo = new MusicasPreferidas("Léo");
+        //musicasPreferidasDoLeo.AdicionarMusicasFavoritas(musicas[487]);
+        //musicasPreferidasDoLeo.AdicionarMusicasFavoritas(musicas[572]);
+        //musicasPreferidasDoLeo.AdicionarMusicasFavoritas(musicas[18]);
+        //musicasPreferidasDoLeo.AdicionarMusicasFavoritas(musicas[666]);
+        //musicasPreferidasDoLeo.AdicionarMusicasFavoritas(musicas[1119]);
+
+        //musicasPreferidasDoLeo.ExibirMusicasFavoritas();
+        ////musicasPreferidasDoLeo.GerarArquivoJson();
+        //musicasPreferidasDoLeo.GerarArquivoTxt();
 
     }
     catch (Exception ex)
